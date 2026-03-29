356959635ad6979aa73504c7e9d7017ba358a382
Willian <willianbardela2131@hotmail.com>
2026-03-29 16:12:47 -0300

    feat: criação do componente HEADER

---
+++ src/components/header.jsx
@@ -0,0 +1,27 @@
+import { LucideTent } from "lucide-react";
+
+export function Header() {
+  return (
+    <div className="bg-gray-200 p-3">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center ml-5 gap-3">
+          <LucideTent className="w-6 h-6 ml-2" />
+          <h1 className="font-semibold">MUI Toolpad</h1>
+          <button className="bg-gray-300 rounded-lg text-gray-400 text-[13px] ml-3 p-1">
+            Beta
+          </button>
+        </div>
+        <p className="text-sm text-gray-600">
+          This is a preview version of the application.
+        </p>
+        <div className="mr-5">
+          <button className="bg-blue-500 text-white px-3 py-1 rounded flex items-center gap-2 text-sm">
+            Edit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
