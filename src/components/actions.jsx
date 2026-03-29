b3146a98752ae81c7d5d6cf66b87a592f6b7ec6e
Willian <willianbardela2131@hotmail.com>
2026-03-29 19:16:04 -0300

    feat: criacao do componente actions

--- src/components/actions.jsx
+++ src/components/actions.jsx
@@ -1,5 +1,22 @@
+import { LucideTrash } from "lucide-react";
+
 export function Actions() {
-  return <h1>Testando as Actions</h1>;
+  return (
+    <div className="mt-5">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-3xl">Cliente List</h1>
+        <div className="flex gap-3">
+          <button className="bg-gray-200 p-2 text-cyan-700 rounded-md flex items-center justify-center font-semibold cursor-pointer hover:bg-gray-300">
+            <span className="font-bold text-2xl mr-2">+</span>Cadastrar Cliente
+          </button>
+          <button className="bg-red-800 text-white rounded-md p-2 flex items-center justify-center cursor-pointer hover:bg-red-700">
+            <LucideTrash className="w-5 h-5 ml-2" />
+            Deletar Cliente
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Actions;
